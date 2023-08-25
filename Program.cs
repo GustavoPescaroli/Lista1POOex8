4b0d15c407e38bd1edc9795b3462b14f6e12c5dd
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:52:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaaPOOex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior a; 
+                a = new Maior();
+
+            Console.WriteLine("x");
+            a.setx(double.Parse(Console.ReadLine()));
+            Console.WriteLine("y");
+            a.sety(double.Parse(Console.ReadLine())) ;
+
+            a.calcular();
+
+
+        }
+    }
+}
